1f0a13b77511874790c0354e837fe1596c863ef6
ibrataha8 <115188113+ibrataha8@users.noreply.github.com>
2023-03-02 10:41:39 +0100

    Update sc.js

--- sc.js
+++ sc.js
@@ -4,6 +4,8 @@ let nbre1
 let nbre2
 let resi = document.querySelector('#resi');
 let score = 0
+let essai = 0
+let essayer = document.querySelector("#essayer")
 
 function change2() {
     nbre1  = Math.floor(Math.random() * 10);
@@ -16,10 +18,12 @@ function change1() {
         resi.innerHTML="Good"
         resi.style.color="green"
         score++
+        essai++
     }
     else{
         resi.innerHTML="Bad"
         resi.style.color="red"
+        essai++
 
     }
 
@@ -32,12 +36,16 @@ function change1() {
     change2()
 }
 function change3() {
-    resi.innerHTML="Votre score est :"+score
+    resi.innerHTML="Votre score est :"+score+"<br>"
     resi.style.color="blue"
+    essayer.innerHTML="Votre essai est :"+essai
+    essayer.style.color="rgb(94, 3, 3)"
 }
 function change4() {
     score=0
+    essai=0
     res.value=""
     resi.innerHTML=""
+    essayer.innerHTML=""
     change2()
 }
